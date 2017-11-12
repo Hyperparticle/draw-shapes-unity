d87c3b1e62bd2922d85edc87869b431eec7aa290
hyperparticle <hyperparticle@gmail.com>
2017-11-12 19:38:44 +0100

    1 - draw simple polygon mesh

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -11,21 +11,4 @@ public static class Util
     {
         return System.Array.ConvertAll<Vector2, Vector3>(vectors, v => v);
     }
-
-    /// <summary>
-    /// Extension that, given a collection of vectors, returns a centroid 
-    /// (i.e., an average of all vectors) 
-    /// </summary>
-    public static Vector2 Centroid(this ICollection<Vector2> vectors)
-    {
-        return vectors.Aggregate((agg, next) => agg + next) / vectors.Count();
-    }
-
-    /// <summary>
-    /// Extension returning the absolute value of a vector
-    /// </summary>
-    public static Vector2 Abs(this Vector2 vector)
-    {
-        return new Vector2(Mathf.Abs(vector.x), Mathf.Abs(vector.y));
-    }
 }
